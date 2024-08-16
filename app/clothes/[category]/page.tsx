6aa8097f788a09ef80d8ea7900d03b8a16d71761
tommaenhout <tom.maenhout@gmail.com>
2024-08-16 18:35:20 -0300

    api working

--- app/clothes/[category]/page.tsx
+++ app/clothes/[category]/page.tsx
@@ -9,9 +9,15 @@ export async function generateMetadata ({params} : Params) {
     }
 }
 
-const ClothesPage : NextPage <Params> = ({params}) => {
+const ClothesPage : NextPage <Params> = async ({params}) => {
     const { category } = params;
+    
+    const response = await fetch(`http://localhost:3000/api/clothes/${category}`);
+    const { filteredProducts } = await response.json();
 
+    console.log(filteredProducts);
+
+    
     return (
         <div>
             <Products category={category}/>
